b8391b9cb20847281fd6466d0faaa1ddc58149e9
Renee <rmui10@stuy.edu>
2020-09-25 20:45:28 -0400

    Add krewes.py in 05 directory

---
+++ 05/krewes.py
@@ -0,0 +1,12 @@
+import random
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDIE', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+group = random.choice(list(KREWES.values()))
+student = random.choice(group)
+
+print(student)
