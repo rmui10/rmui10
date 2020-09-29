1d97d9eb3df191f5c929bb6d3b2240c90d48142b
Renee <rmui10@stuy.edu>
2020-09-29 14:44:57 -0400

    Change to group code

--- 05/krewes.py
+++ 05/krewes.py
@@ -1,12 +1,24 @@
+# Team SHWORM (Stella Oh, Helenda Williams, Renee Mui)
+# SoftDev
+# K05 -- Teamwork, but Better This Time
+# 2020-09-26
+
+# A program that will print the name of a randomly-selected student
+# from team (orpheus, rex, or endymion)
+
 import random
 
+# Dictionary of team (str) and names in each team (list)
 KREWES = {
     'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
     'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
-    'endymion': ['JASON', 'DEAN', 'MADDIE', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+    'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
 }
 
-group = random.choice(list(KREWES.values()))
-student = random.choice(group)
+# Prints the name of a randomly-selected student from team (orpheus, rex, or endymion)
+def pick_name(dictionary):
+    # picks random team then a random name from that chosen team
+    print(random.choice(random.choice(list(dictionary.values()))))
 
-print(student)
+#test-run of random name
+pick_name(KREWES)
