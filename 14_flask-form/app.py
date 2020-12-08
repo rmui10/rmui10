675d59d1c94d24f103405249b991cab13bf89162
Renee <rmui10@stuy.edu>
2020-12-08 15:23:31 -0500

    Add K14 folder and files

---
+++ 14_flask-form/app.py
@@ -0,0 +1,22 @@
+#Team RIID - Ishita Gupta, Dragos Lup, Renee Mui, Ian Chen-Adamczyk
+#SoftDev
+#K14 -- Form and Function/Echo user output received from html form 
+#2020-12-8
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__) 
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    username = request.form['username']
+    return render_template('response.html', name = username)
+    
+if __name__ == "__main__": 
+    app.debug = True 
+    app.run()
